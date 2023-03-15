f781e9e6c6f9e676355c0852d7b68d8e3bf7b90d
Jc Jeacanie Llido <68636366+Imamouse@users.noreply.github.com>
2023-03-14 19:31:33 -1100

    Added the simple code.

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+
+# print welcome message
+print("Welcome to the Band Name Generator\n")
+
+# input city
+city = input("What's name of the city you grew up in? \n")
+
+# input pet name
+pet = input("What's the name of the pet? \n")
+
+# print band name
+print(f"The band name is {city}_{pet}.\n")
+
+time.sleep(10)
